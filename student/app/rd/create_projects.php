dce647b815d3b281a1848e3cb474c949fe610d73
Remmert <remmert_kloppenburg@msn.com>
2014-09-30 14:18:15 +0200

    development pagina

---
+++ student/app/rd/create_projects.php
@@ -0,0 +1,87 @@
+
+
+
+<div class="container">
+	<p>hier mag je niet komen als je niet ingelogd bent en de rol als development hebt...</p>
+
+	<form method="post" action="index.php" role="form" class="users-form col-md-8 col-md-offset-2">
+		<fieldset>
+		<legend>Creëer project</legend>
+
+		<ul>
+			<?php  
+			// succes of fail message
+			if (isset($_GET['msg'])) {
+				echo '<li>' .  htmlspecialchars($_GET['msg']) . '</li>';
+			}
+			?>
+			</ul>
+
+			<div class="form-group col-md-5">
+				<label for="company_name">Company name</label>
+				<input type="company_name" name="company_name" id="company_name" class="form-control">	
+			</div>
+			<div class="form-group col-md-2">
+				<label for="address">Address</label>
+				<input type="address" name="address" id="address" class="form-control">	
+			</div>
+			<div class="form-group col-md-5">
+				<label for="postcode">Postcode</label>
+				<input type="postcode" name="postcode" id="postcode" class="form-control">	
+			</div>
+			<div class="form-group col-md-6">
+				<label for="residence">Residence</label>
+				<input type="residence" name="residence" id="residence" class="form-control">	
+			</div>
+			<div class="form-group col-md-6">
+				<label for="contact_persons">Contact persons</label>
+				<input type="contact_persons" name="contact_persons" id="contact_persons" class="form-control">	
+			</div>
+			<div class="form-group col-md-6">
+				<label for="telephone_number">Telephone Number</label>
+				<input type="telephone_number" name="telephone_number" id="telephone_number" class="form-control">
+			</div>
+			<div class="form-group col-md-6">
+				<label for="fax_number">Fax Number</label>
+				<input type="fax_number" name="fax_number" id="fax_number" class="form-control">
+			</div>
+			<div class="form-group col-md-6">
+				<label for="email">Email</label>
+				<input type="email" name="email" id="email" class="form-control">
+			</div>
+			<div class="form-group col-md-6">
+				<label for="maintenance_contract">Maintenance Contract</label>
+				<input type="maintenance_contract" name="maintenance_contract" id="maintenance_contract" class="form-control">
+			</div>
+			<div class="form-group col-md-6">
+				<label for="open_projects">Open projects</label>
+				<input type="open_projects" name="open_projects" id="open_projects" class="form-control">
+			</div>
+			<div class="form-group col-md-6">
+				<label for="applications">Applications</label>
+				<input type="applications" name="applications" id="applications" class="form-control">
+			</div>
+			<div class="form-group col-md-6">
+				<label for="hardware">Hardware</label>
+				<input type="hardware" name="hardware" id="hardware" class="form-control">
+			</div>
+			<div class="form-group col-md-6">
+				<label for="software">Software</label>
+				<input type="software" name="software" id="software" class="form-control">
+			</div>
+			<div class="form-group col-md-6">
+				<label for="appointments">Appointments</label>
+				<input type="appointments" name="appointments" id="appointments" class="form-control">
+			</div>
+			<div class="form-group col-md-6">
+				<label for="internal_contact_person">Internal contact person</label>
+				<input type="internal_contact_persond" name="internal_contact_person" id="internal_contact_person" class="form-control">
+			</div>
+			<
+			<div class="form-group col-md-12">
+				<input type="submit" value="Maak project aan" name="create_projects" class="btn btn-large">
+			</div>
+		</fieldset>
+	</form>
+</div>
+
--- student/app/rd/index.php
+++ student/app/rd/index.php
@@ -1,26 +1,65 @@
-<?php include '../templates/header.php'; ?>
 
-<div class="header">
-	<h1>projects</h1>
-</div>
-<div class="projects" >
-	<div class="container" >
-		<table class="table">
-			<thead>
-				<tr>
-					<th>project naam</th>
-					<th>opdrachtgever</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php 
-					$sql ="SELECT ALL FROM projecten";
 
-				 ?>
-			</tbody>
-			
-		</table>
-	</div>
-</div>
+<?php
 
-<?php include '../templates/footer.php'; ?>
+require '../../config/config.php';
+if ( isset($_POST['create_projects']) ) 
+
+$company_name                =mysqli_real_escape_string($con, $_POST['company_name']);
+$address 		             =mysqli_real_escape_string($con, $_POST['address']);
+$postcode                    =mysqli_real_escape_string($con, $_POST['postcode']);
+$residence                   =mysqli_real_escape_string($con, $_POST['residence']);
+$contact_persons             =mysqli_real_escape_string($con, $_POST['contact_persons']);
+$telephone_number            =mysqli_real_escape_string($con, $_POST['telephone_number']);
+$fax_number                  =mysqli_real_escape_string($con, $_POST['fax_number']);
+$email                       =mysqli_real_escape_string($con, $_POST['email']);
+$maintenance_contract        =mysqli_real_escape_string($con, $_POST['maintenance_contract']);
+$open_projects               =mysqli_real_escape_string($con, $_POST['open_projects']);
+$applications                =mysqli_real_escape_string($con, $_POST['applications']);
+$hardware                    =mysqli_real_escape_string($con, $_POST['hardware']);
+$software                    =mysqli_real_escape_string($con, $_POST['software']);
+$appointments                =mysqli_real_escape_string($con, $_POST['appointments']);
+$internal_contact_person     =mysqli_real_escape_string($con, $_POST['internal_contact_person']);
+
+
+
+
+
+$sql = "INSERT INTO projecten (company_name, address, postcode, residence, contact_persons, telephone_number, fax_number, email, maintenance_contract,
+ open_projects, applications, hardware, software, appointments, internal_contact_person)
+			VALUES (
+				'$company_name',
+				'$address',
+				'$postcode',
+				'$residence',
+				'$contact_persons',
+				'$telephone_number',
+				'$fax_number',
+				'$email',
+				'$maintenance_contract',
+				'$open_projects',
+				'$applications',
+				'$hardware',
+				'$software',
+				'$appointments',
+				'$internal_contact_person',
+
+						)";
+
+	$query = mysqli_query($con, $sql);
+
+	if (!$query) {
+		$msg = urlencode(trigger_error('query niet gelukt. geprobeerde query was ' . $sql));
+		header('location: ../development/create_projects.php?msg='.$msg);	
+	}
+
+	$msg = urlencode('project  is succesvol toegevoegd.');
+	header ('location: ../development/create_projects.php?msg='.$msg);
+
+				
+
+
+
+
+
+?>
