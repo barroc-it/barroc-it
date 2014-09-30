22e011381f790c5afb455bd40462351edb23a1dd
regilio dielemans <regilio_dielemans@hotmail.com>
2014-09-30 14:17:39 +0200

    login page afbeelding

--- student/app/login.php
+++ student/app/login.php
@@ -1,33 +1,33 @@
 <?php require 'templates/header.php'; ?>
+<div class="login">
+	<div class="container">
+		<form method="post" action="controllers/authcontroller.php" role="form" class="login-form col-md-4 col-md-offset-4">
+			<fieldset>
+			<legend><h2>Login</h2></legend>
 
-<div class="container">
-	<form method="post" action="controllers/authcontroller.php" role="form" class="login-form col-md-4 col-md-offset-4">
-		<fieldset>
-		<legend><h2>Login</h2></legend>
+				<ul>
+					<?php  
+					// succes of fail message
+					if (isset($_GET['msg'])) {
+						echo '<li>' .  htmlspecialchars($_GET['msg']) . '</li>';
+					}
+					?>
+				</ul>
 
-			<ul>
-				<?php  
-				// succes of fail message
-				if (isset($_GET['msg'])) {
-					echo '<li>' .  htmlspecialchars($_GET['msg']) . '</li>';
-				}
-				?>
-			</ul>
-
-			<div class="form-group">
-				<label for="email">Email</label>
-				<input type="email" name="email" id="email" class="form-control">	
-			</div>
-			<div class="form-group">
-				<label for="password">Password</label>
-				<input type="password" name="password" id="password" class="form-control">
-			</div>
-			<div class="form-group">
-				<input type="submit" name="authUser" value="Login" class="btn btn-large">
-			</div>
-		</fieldset>
-	</form>
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input type="email" name="email" id="email" class="form-control">	
+				</div>
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" name="password" id="password" class="form-control">
+				</div>
+				<div class="form-group">
+					<input type="submit" name="authUser" value="Login" class="btn btn-large">
+				</div>
+			</fieldset>
+		</form>
+	</div>
 </div>
-
 
 <?php require 'templates/footer.php'; ?>
