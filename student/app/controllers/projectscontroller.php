ec2ce97acd37563c09dc6942d3a9562a48002419
regilio dielemans <regilio_dielemans@hotmail.com>
2014-10-03 11:55:45 +0200

    projectcontroller

---
+++ student/app/controllers/projectscontroller.php
@@ -0,0 +1,32 @@
+<?php 
+require '../database.php';
+
+if (isset($_POST['createUser'] ) ) {
+
+	$username = mysqli_real_escape_string($con, $_POST['username']);
+	$skype_id = mysqli_real_escape_string($con, $_POST['skype_id']);
+	
+	$sql = "INSERT INTO contacten1 (naam, skype_id) VALUES ('$username', '$skype_id')";
+
+	if( $query = mysqli_query($con, $sql)){
+		$msg = "gebruiker is succesvol aangemaakt";
+		header('location: ../../index.php');
+	}else{
+		echo "kan de query niet uitvoeren";
+	}
+}
+
+
+if (isset($_GET['delete'])){
+	$id = $_GET['id'];
+	$sql= "DELETE FROM contacten1 WHERE id = '$id'";
+
+	if ($query = mysqli_query($con, $sql)) {
+		header("location: ../../index.php");
+
+	}else{
+		echo "foutje met de verwijder query..";
+	}
+}
+
+?>
