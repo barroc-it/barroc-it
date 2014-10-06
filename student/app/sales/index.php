ab60881ddf550a7900af724565848b0240ead292
regilio dielemans <regilio_dielemans@hotmail.com>
2014-10-06 16:25:31 +0200

    btn toevoegen

--- student/app/sales/index.php
+++ student/app/sales/index.php
@@ -9,7 +9,32 @@
 		
 	</div>
 	<div class="customers">
-		
+		<table class="table table-striped">
+			<input type="input_customer" value="toevoegen	" name="input_customers" class="btn btn-primary col-md-2 col-md-offset-10">
+		<tr>
+			<th>companyName</th>
+			<th>address</th>
+			<th>postcode</th>
+			<th>residence</th>
+			<th>telephonenumber</th>
+			<th>email</th>
+		</tr>
+			<?php 
+			$sql = "SELECT * FROM customers";
+			$query = mysqli_query($con, $sql);
+
+			while ($row = mysqli_fetch_assoc($query)) {
+				echo "<tr>";
+					echo "<td>" . $row['companyName'] . "</td>";
+					echo '<td>' . $row['address'] . '</td>';
+					echo '<td>' . $row['postcode'] . '</td>';
+					echo '<td>' . $row['residence'] . '</td>';
+					echo '<td>' . $row['telephoneNumber'] . '</td>';
+					echo '<td>' . $row['email'] . '</td>';
+					
+				echo "</tr>";
+			}
+		?>
 
 
 
