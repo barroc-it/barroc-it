9d3488c60212b5dee5e8cc8c9c185e9b94cfcb89
Remmert <remmert_kloppenburg@msn.com>
2014-10-06 16:25:39 +0200

    begonnen aan financien pagina

--- student/app/finance/index.php
+++ student/app/finance/index.php
@@ -1,1 +1,42 @@
-<?php include 'templates/header.php'; ?>
+<?php 
+	include '../templates/header.php'; 
+	require '../../config/config.php';
+?>
+	
+	<div class="container">
+		<h1>Finance</h1>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>Companyname</th>
+					<th>bank account number</th>
+					<th>Credit</th>
+					<th>Revenue ammount</th>
+					<th>Limit</th>
+					<th>Ledger account</th>
+					<th>BKR</th>
+				</tr>
+			</thead>
+					
+			<tbody class="finance">
+				<?php 
+					$sql = "SELECT * FROM customers" ;
+					$query = mysqli_query($con, $sql);
+
+					while ($row = mysqli_fetch_assoc($query)) {
+						echo '<tr>';
+						echo '<td>' . $row['companyName'] . '</td>';
+						echo '<td>' . $row['banknumber'] . '</td>';
+						echo '<td>' . $row['krediet'] . '</td>';
+						echo '<td>' . $row['omzetbedrag'] . '</td>';
+						echo '<td>' . $row['limiet'] . '</td>';
+						echo '<td>' . $row['grootboekingsnummer'] . '</td>';
+						echo '<td>' . $row['BKR'] . '</td>';
+					
+						echo '</tr>';
+						}
+					?>
+	
+<?php 
+	include '../templates/footer.php'; 
+	?>
