2ab60ef4f4ed38e8911dfd3c700608d40c3ee166
regilio dielemans <regilio_dielemans@hotmail.com>
2014-10-13 15:28:07 +0200

    search index

--- student/app/sales/index.php
+++ student/app/sales/index.php
@@ -15,18 +15,23 @@ header("location:../login.php");
 	<div class="page-header">
 		<h2>sales</h2>
 		<h3>customers</h3>
-		
-		
 	</div>
+	
+	<a class="btn btn-primary col-md-2" href="add_custumers.php">toevoegen</a>
+	<a class="btn btn-info col-md-2 col-md-offset-8" href="logout.php">logout</a>
+	<br>
+	<br>
+	<br>
+		<div class="">
+			<form  method="GET" action="indexsearch.php" id="searchform" name="search"> 
+			    <input id="search-bar" type="text" class="form-control inputsearch" placeholder="Search..." name="search"> 
+			    <input type="submit" class="search-btn"> 
+			</form> 
+		</div>
+	<br>
+
 	<div class="customers">
-		<table class="table table-striped">
-				<br>
-				<a class="btn btn-primary col-md-2" href="add_custumers.php">toevoegen</a>
-				<a class="btn btn-info col-md-2 col-md-offset-8" href="logout.php">logout</a>
-
-
-			<br>
-			<br>
+	<table class="table table-striped">
 		<tr>
 			<th>companyName</th>
 			<th>address</th>
