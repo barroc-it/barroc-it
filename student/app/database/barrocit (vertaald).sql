8340aeb40f73ba851036e55859224731109901bc
koentjeh <koen-debont@hotmail.com>
2014-10-13 15:34:47 +0200

    database

--- student/app/database/barrocit (vertaald).sql
+++ student/app/database/barrocit (vertaald).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: 127.0.0.1
--- Gegenereerd op: 13 okt 2014 om 13:48
+-- Gegenereerd op: 13 okt 2014 om 15:34
 -- Serverversie: 5.6.17
 -- PHP-versie: 5.5.12
 
@@ -19,6 +19,23 @@ SET time_zone = "+00:00";
 --
 -- Databank: `barrocit`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `appointments`
+--
+
+CREATE TABLE IF NOT EXISTS `appointments` (
+  `appointment NR` int(11) NOT NULL AUTO_INCREMENT,
+  `customerNR` int(10) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `description` varchar(80) NOT NULL,
+  PRIMARY KEY (`appointment NR`),
+  KEY `customersNR` (`customerNR`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -61,6 +78,92 @@ CREATE TABLE IF NOT EXISTS `customers` (
 INSERT INTO `customers` (`customerNR`, `companyName`, `address`, `postcode`, `residence`, `telephoneNumber`, `email`, `amountOfInvoices`, `openProjects`, `appointments`, `internalContact`, `dateAction`, `lastcontactDate`, `nextAction`, `contactPerson`, `BTW`, `salesAmount`, `balance`, `credit`, `limit`, `largeReservationNumber`, `offerStatus`, `prostpect`, `bkr_control`) VALUES
 (1, 'test', 'zooi', '1111aa', '', 0, '', 0, 0, '', '', '0000-00-00', '0000-00-00', '', 'name', 0, NULL, 1500, '', 0, '', '', '', 0),
 (2, 'bedrijfsnaam', 'namenstraat', '4363fg', 'raamsdonkcity', 946582, 'email@hotmail.com', 2, 2, '', '', '0000-00-00', '0000-00-00', '', 'naam', 0, NULL, 200, '', 0, '', '', '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `invoices`
+--
+
+CREATE TABLE IF NOT EXISTS `invoices` (
+  `invoices NR` int(11) NOT NULL AUTO_INCREMENT,
+  `projectNR` int(10) NOT NULL,
+  `date` date NOT NULL,
+  `amount` int(10) NOT NULL,
+  `paid` tinyint(1) NOT NULL,
+  PRIMARY KEY (`invoices NR`),
+  KEY `projectsNR` (`projectNR`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `projects`
+--
+
+CREATE TABLE IF NOT EXISTS `projects` (
+  `projectNR` int(11) NOT NULL AUTO_INCREMENT,
+  `customerNR` int(10) NOT NULL,
+  `maintenance_contract` tinyint(1) NOT NULL,
+  `software` varchar(50) NOT NULL,
+  `hardware` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`projectNR`),
+  KEY `customerNR` (`customerNR`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `projects`
+--
+
+INSERT INTO `projects` (`projectNR`, `customerNR`, `maintenance_contract`, `software`, `hardware`, `description`, `active`) VALUES
+(1, 1, 1, 'dsjkdsj', 'grmrmm', 'dkjdkfjdk', 0),
+(2, 2, 2, 'gyhujm', 'rfthj', 'ghuj', 0),
+(11, 2, 2, 'dfghjm', 'sdrfgthj', 'sdfgth', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `name` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `gebruikersrol` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `users`
+--
+
+INSERT INTO `users` (`name`, `password`, `gebruikersrol`) VALUES
+('sales', 'sales', 1),
+('finance', 'finance', 2),
+('development', 'development', 3);
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `appointments`
+--
+ALTER TABLE `appointments`
+  ADD CONSTRAINT `appointments-customers` FOREIGN KEY (`customerNR`) REFERENCES `customers` (`customerNR`) ON UPDATE CASCADE;
+
+--
+-- Beperkingen voor tabel `invoices`
+--
+ALTER TABLE `invoices`
+  ADD CONSTRAINT `project-invoice` FOREIGN KEY (`projectNR`) REFERENCES `projects` (`projectNR`) ON UPDATE CASCADE;
+
+--
+-- Beperkingen voor tabel `projects`
+--
+ALTER TABLE `projects`
+  ADD CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`customerNR`) REFERENCES `customers` (`customerNR`) ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
