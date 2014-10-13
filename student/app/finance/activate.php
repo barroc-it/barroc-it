547a4e0837d77139bfa60e95f7d7c2fe8ba3c287
Remmert <remmert_kloppenburg@msn.com>
2014-10-13 14:23:13 +0200

    finance gewerkt

--- student/app/finance/activate.php
+++ student/app/finance/activate.php
@@ -3,37 +3,64 @@
 	require '../../config/config.php';
 
 
-	if(isset($_GET['id'])){
 
+
+	if ( isset($_GET['id']) ) {
 		$id = $_GET['id'];
-	$sql = "SELECT * FROM invoices WHERE `invoicesNR = $id";
-	$query = mysqli_query($con, $sql);
+		$sql = "SELECT * FROM invoices WHERE invoicesNR = '$id' ";
 
-
-	}
+		if (!$query = mysqli_query($con, $sql)) {
+			echo '<a href="index.php"><button>Kan selectie niet uitvoeren, Klik hier om terug te gaan.</button></a>';
+			die();
+			}
+		$row = mysqli_fetch_assoc($query);
+		}
+	
 ?>
 	
+	<div class="container">
+		<h1>Finance panel Activate</h1>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>Invoice duration</th>
+					<th>Quintity</th>
+					<th>Description</th>
+					<th>Price</th>
+					<th>BTW</th>
+					<th>Amount</th>
+					<th>Edit</th>
+					<th>Deactivate</th>
+				</tr>
+			</thead>
+					
+			<tbody class="finance">
+				<?php 
+				
+
+	
+
+
+
+
+	$sql = "SELECT * FROM invoices";
+		$query = mysqli_query($con, $sql);
+					while ($row = mysqli_fetch_assoc($query)) {
+						echo '<tr>';
+						echo '<td>' . $row['datum'] . '</td>';
+						echo '<td>' . $row['hoeveelheid'] . '</td>';
+						echo '<td>' . $row['description'] . '</td>';
+						echo '<td>' . $row['bedrag'] . '</td>';
+						echo '<td>' . $row['btw'] . '</td>';
+						echo '<td>' . $row['amount'] . '</td>';
+						echo '<td><a href="activateEdit.php">Edit</a></td>';
+						echo '<td><a href="deactivefinance.php?invoicesNR=' . $row['projectsNR'] .  '&delete=true">Deactivate</a>';
+					
+						echo '</tr>';
+						}
+					?>
+
 <?php 
-
-
-	if (!$con) {
-			echo 'Kan geen connectie maken met de database';
-			die();
-		}
-
-			if ( isset($_GET['invoicesNR']) ) {
-				$projectsNR = $_GET['invoicesNR'];
-				$sql = "UPDATE invoices SET actief = 0 WHERE invoicesNR= '$invoicesNR'";
-
-				if (!$query = mysqli_query($con, $sql)) {
-					echo 'Kan helaas niet updaten...';
-					die();
-				} else {
-					$msg = urlencode('Invoices changed!');
-					header('location: deactivefinance.php?invoicesNR=' . $row['invoicesNR'] . '');
-				}
-			}
-?>
-
-</body>
-</html>
+	include '../templates/footer.php'; 
+	?>
+	<a href="index.php">Back</a>
