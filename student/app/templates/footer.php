ce8396d3334731a9c5b135a22c3ce32d098d61f8
koentjeh <koen-debont@hotmail.com>
2014-10-14 16:50:13 +0200

    comments werken

--- student/app/templates/footer.php
+++ student/app/templates/footer.php
@@ -1,29 +1,67 @@
-
+<?php
+	require '../../config/config.php';
+?>
 <link rel="stylesheet" href="../assets/css/style.css">
+<link rel="stylesheet" type="text/css" href="http://bootswatch.com/paper/bootstrap.min.css">
 
-<form action="" method="POST" class="comment">
-	<div class="form-group col-sm-12">
-		<label for="name">Name</label>
-		<input type="text" value='' class="form-control" name="name" id="name">
-	</div>
-	<div class="form-group col-sm-12">
-		<label for="department">Department</label>
-		<input type="text" value='' class="form-control" name="deparment" id="department">
-	</div>
-	<div class="form-group col-sm-12">
-		<label for="postcode">Postcode</label>
-		<textarea rows="4" cols="50" value='' class="form-control" name="postcode" id="postcode"></textarea>
-	</div>
-	<div class="form-group col-sm-12">
-		<label for="telefoonnummer">Telefoonnummer</label>
-		<input type="text" value='' class="form-control" name="telefoonnummer" id="telefoonnummer">
-	</div>
-	<div class="form-group col-sm-12">
-		<input name="submit" type="submit" value="Bewerken" class="btn btn-default btn-lg btn-block">
-	</div>
-</form>
+<div class="footerclass">
+
+	<form action="footer.php" method="POST" class="commentForm">
+		<div class="form-group col-sm-12">
+			<label for="name">Name</label>
+			<input type="text" value='' class="form-control" name="name" id="name">
+		</div>
+		<div class="form-group col-sm-12">
+			<label for="department">Department</label>
+				<select name="departmentchoice" class="form-control">
+					<option value="sales" name="sales" id="sales">Sales</option>
+					<option value="finance" name="finance" id="finance">Finance</option>
+					<option value="development" name="development" id="development">Development</option>
+				</select>
+		</div>
+		<div class="form-group col-sm-12">
+			<label for="comment">comment</label>
+			<textarea rows="4" cols="50" value='' class="form-control" name="comment" id="comment"></textarea>
+		</div>
+		<div class="form-group col-sm-12">
+			<input name="submit" type="submit" value="Place comment" class="btn btn-primary">
+		</div>
+	</form>
 
 
+	<?php
+
+	if (isset($_POST['submit'] ) ) {
+			
+			$name = $_POST['name'];
+			$department = $_POST['departmentchoice'];
+			$comment = $_POST['comment'];
+			$datetime = Datetime('Y-m-d');
+			$sql = "INSERT INTO comments (name, department, comment, datetime ) VALUES ('$name', '$department', '$comment', '$datetime')";
+			
+			if( $query = mysqli_query($con, $sql)) {
+				header('location: footer.php');
+			} else {
+				echo "kan de query niet uitvoeren";
+			}
+		}
+
+		$sql = "SELECT * FROM comments ORDER BY datetime DESC ";
+		$query = mysqli_query($con, $sql);
+
+		while ($row = mysqli_fetch_assoc($query)) {
+			echo '<div class="container">';
+			echo '<table class="table tablewidth tablebottom">';
+
+			echo '<thead><tr><th><h3>' . $row['name'] . ' has a comment:<span class="text-muted commentdate">' . $row['datetime'] . '</span></h3></th></tr></thead>';				
+	?>
+				<tr><td><b>Department:</b>
+				<?php echo $row['department'] . '</td></tr>'; ?>
+				<tr><td><b>Comment:</b><br>
+				<?php echo $row['comment'] . '</td></tr>';
+				}
+	?>
+</div>
 
 </body>
 </html>
