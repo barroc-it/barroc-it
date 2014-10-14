e841e130a3ebc2c335eb098de193217ba3cb6b1d
regilio dielemans <regilio_dielemans@hotmail.com>
2014-10-14 14:22:20 +0200

    css

--- student/app/sales/index.php
+++ student/app/sales/index.php
@@ -17,17 +17,15 @@ header("location:../login.php");
 		<h3>customers</h3>
 	</div>
 	
-	<a class="btn btn-primary col-md-2" href="add_custumers.php">toevoegen</a>
-	<a class="btn btn-info col-md-2 col-md-offset-8" href="logout.php">logout</a>
-	<br>
-	<br>
-	<br>
-		<div class="">
-			<form  method="GET" action="indexsearch.php" id="searchform" name="search"> 
-			    <input id="search-bar" type="text" class="form-control inputsearch" placeholder="Search..." name="search"> 
-			    <input type="submit" class="search-btn"> 
-			</form> 
-		</div>
+	<a class="btn" href="add_custumers.php">toevoegen</a>
+	
+
+	
+<!-- 	<form  class="col-md-4" method="GET" action="indexsearch.php" id="searchform" name="search"> 
+	    <input id="search-bar" type="text" class="form-control inputsearch" placeholder="Search..." name="search"> 
+	    <input type="submit" class="search-btn"> 
+	</form> 
+	<a class="btn btn-info col-md-2" href="logout.php">logout</a> -->
 	<br>
 
 	<div class="customers">
